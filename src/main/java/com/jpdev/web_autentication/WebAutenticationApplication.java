c3ed45ad85d74999b2b3b80ddd97ee415c5ace71
Joao-Pedro-J <matheusitalo125@gmail.com>
2026-02-23 12:28:46 -0300

    first commit

---
+++ src/main/java/com/jpdev/web_autentication/WebAutenticationApplication.java
@@ -0,0 +1,13 @@
+package com.jpdev.web_autentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAutenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpdev/web_autentication/WebAutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.web_autentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAutenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
